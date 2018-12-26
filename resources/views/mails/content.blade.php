30c868353b0eeed2d462bc22e26647b0aae22eb6
Rio Gunawan <riogunawan8967uajy@gmail.com>
2018-12-26 17:03:05 +0700

    email

--- resources/views/mails/content.blade.php
+++ resources/views/mails/content.blade.php
@@ -3,19 +3,19 @@
         <table style="width: 100%">
             <tr style="border: none">
                 <td>Name</td>
-                <td>: $dataEmail->name</td>
+                <td>: {{$dataEmail->name}}</td>
             </tr>
             <tr style="border: none">
                 <td>Email</td>
-                <td>: $dataEmail->email</td>
+                <td>: {{$dataEmail->email}}</td>
             </tr>
             <tr style="border: none">
                 <td>Subject</td>
-                <td>: $dataEmail->subject</td>
+                <td>: {{$dataEmail->subject}}</td>
             </tr>
             <tr style="border: none">
                 <td>Message</td>
-                <td>: $dataEmail->message</td>
+                <td>: {{$dataEmail->message}}</td>
             </tr>
         </table>
     </body>

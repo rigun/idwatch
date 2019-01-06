f1ad9c5e8eed34a2f9ef1ce8e34f9a22443d223a
Rio Gunawan <riogunawan8967uajy@gmail.com>
2019-01-06 16:15:29 +0700

    view

--- app/Exports/ReportItem.php
+++ app/Exports/ReportItem.php
@@ -29,7 +29,7 @@ class ReportItem implements FromView, ShouldAutoSize, WithEvents
                 $cellRange = 'A1:F1'; // All headers
                 // $event->sheet->getDelegate()->getStyle($cellRange)->getFont()->setSize(14);
                 $event->sheet->getDelegate()->setAutoFilter($cellRange);
-                $event->sheet->getDelegate()->getStyle('B:B')->getAlignment()->setWrapText(true);
+                $event->sheet->getDelegate()->getStyle('B1:B2000')->getAlignment()->setWrapText(true);
             },
         ];
     }

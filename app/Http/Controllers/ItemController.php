4be5b30862122ad9ec51bb5c2bdfcf30e1f171f6
Rio Gunawan <riogunawan8967uajy@gmail.com>
2019-01-09 13:45:25 +0700

    revisi 2

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -45,7 +45,7 @@ class ItemController extends Controller
         return "Success";
     }
     public function search($search){
-            return response()->json(Item::where(['name', 'LIKE', '%'.$search.'%'])->orWhere(['merk', 'LIKE', '%'.$search.'%'])->orWhere(['description', 'LIKE', '%'.$search.'%'])->with('picture','category')->get());
+            return response()->json(Item::where(['name', 'LIKE', '%'.$search.'%'])->with('picture','category')->get());
     }
   
     /**

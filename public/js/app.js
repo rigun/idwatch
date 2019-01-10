0617cf758d26297495aef894224c85fb7063491f
Rio Gunawan <riogunawan8967uajy@gmail.com>
2019-01-10 08:20:00 +0700

    revisi 2

--- public/js/app.js
+++ public/js/app.js
@@ -28502,7 +28502,7 @@ exports = module.exports = __webpack_require__(1)(false);
 
 
 // module
-exports.push([module.i, "\n.inner-table tr > td{\r\n    border: none;\n}\r\n", ""]);
+exports.push([module.i, "\n.inner-table tr > td{\r\n    border: none;\n}\n.hidden{\r\n    display: none;\n}\r\n", ""]);
 
 // exports
 
@@ -28513,6 +28513,9 @@ exports.push([module.i, "\n.inner-table tr > td{\r\n    border: none;\n}\r\n", "
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+//
+//
+//
 //
 //
 //
@@ -28692,71 +28695,79 @@ var render = function() {
             _c(
               "tbody",
               _vm._l(_vm.reports, function(report) {
-                return _c("tr", { key: report.id, staticClass: "odd gradeX" }, [
-                  _c("td", [
-                    _vm._v(
-                      "\n                                                " +
-                        _vm._s(report.id) +
-                        "\n                                            "
-                    )
-                  ]),
-                  _vm._v(" "),
-                  _c("td", [
-                    _c(
-                      "table",
-                      {
-                        staticClass: "inner-table",
-                        staticStyle: { border: "none" },
-                        attrs: { width: "100%" }
-                      },
-                      _vm._l(report.detail, function(item) {
-                        return _c("tr", { key: item.id }, [
-                          _c("td", [_vm._v(_vm._s(item.cart.item.name))]),
-                          _vm._v(" "),
-                          _c("td", [_vm._v(_vm._s(item.cart.quantity))])
-                        ])
-                      })
-                    )
-                  ]),
-                  _vm._v(" "),
-                  _c("td", [
-                    _vm._v(
-                      "\n                                                Rp " +
-                        _vm._s(
-                          parseInt(report.total) +
-                            parseInt(report.shipping) -
-                            parseInt(report.diskon)
-                        ) +
-                        "\n                                            "
-                    )
-                  ]),
-                  _vm._v(" "),
-                  _c("td", { staticClass: "center" }, [
-                    report.status == 0 ? _c("span", [_vm._v("-")]) : _vm._e(),
+                return _c(
+                  "tr",
+                  {
+                    key: report.id,
+                    staticClass: "odd gradeX",
+                    class: { hidden: report.status < 3 }
+                  },
+                  [
+                    _c("td", [
+                      _vm._v(
+                        "\n                                                " +
+                          _vm._s(report.id) +
+                          "\n                                            "
+                      )
+                    ]),
                     _vm._v(" "),
-                    report.status == 1 || report.status == 3
-                      ? _c("span", [_vm._v("Transfer Tunai")])
-                      : _vm._e(),
+                    _c("td", [
+                      _c(
+                        "table",
+                        {
+                          staticClass: "inner-table",
+                          staticStyle: { border: "none" },
+                          attrs: { width: "100%" }
+                        },
+                        _vm._l(report.detail, function(item) {
+                          return _c("tr", { key: item.id }, [
+                            _c("td", [_vm._v(_vm._s(item.cart.item.name))]),
+                            _vm._v(" "),
+                            _c("td", [_vm._v(_vm._s(item.cart.quantity))])
+                          ])
+                        })
+                      )
+                    ]),
                     _vm._v(" "),
-                    report.status == 2 || report.status == 4
-                      ? _c("span", [_vm._v("Cash on delivery")])
-                      : _vm._e()
-                  ]),
-                  _vm._v(" "),
-                  _c("td", { staticClass: "center" }, [
-                    _vm._v(
-                      "\n                                                " +
-                        _vm._s(report.created_at) +
-                        "\n                                            "
-                    )
-                  ]),
-                  _vm._v(" "),
-                  _c("td", { staticClass: "center" }, [
-                    report.status < 3
-                      ? _c("span", [_vm._v("Belum terverfikiasi")])
-                      : _c("span", [_vm._v("Terverfikiasi")])
-                  ])
-                ])
+                    _c("td", [
+                      _vm._v(
+                        "\n                                                Rp " +
+                          _vm._s(
+                            parseInt(report.total) +
+                              parseInt(report.shipping) -
+                              parseInt(report.diskon)
+                          ) +
+                          "\n                                            "
+                      )
+                    ]),
+                    _vm._v(" "),
+                    _c("td", { staticClass: "center" }, [
+                      report.status == 0 ? _c("span", [_vm._v("-")]) : _vm._e(),
+                      _vm._v(" "),
+                      report.status == 1 || report.status == 3
+                        ? _c("span", [_vm._v("Transfer Tunai")])
+                        : _vm._e(),
+                      _vm._v(" "),
+                      report.status == 2 || report.status == 4
+                        ? _c("span", [_vm._v("Cash on delivery")])
+                        : _vm._e()
+                    ]),
+                    _vm._v(" "),
+                    _c("td", { staticClass: "center" }, [
+                      _vm._v(
+                        "\n                                                " +
+                          _vm._s(report.created_at) +
+                          "\n                                            "
+                      )
+                    ]),
+                    _vm._v(" "),
+                    _c("td", { staticClass: "center" }, [
+                      report.status < 3
+                        ? _c("span", [_vm._v("Belum terverfikiasi")])
+                        : _c("span", [_vm._v("Terverfikiasi")])
+                    ])
+                  ]
+                )
               })
             )
           ]

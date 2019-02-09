c54c823e2d9f84af59435683443c810ab1b3b5f1
Rio Gunawan <riogunawan8967uajy@gmail.com>
2019-02-09 09:07:11 +0700

    update

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -66,7 +66,7 @@ class TransactionController extends Controller
         $P_month = 0;
         $P_year = 0;
         $pending = Transaction::where([['status','<',3],['notes','!=',null]])->count();
-        $barang = Item::count();
+        $barang = Item::where('status',1)->count();
         $now = new DateTime();
         foreach($transactions as $transaction){
             if($transaction->created_at->format('Y') == $now->format('Y')){
